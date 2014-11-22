45f7d31b339533eba3a5dd741bbd42070479f9b1
Daiwei <daiweilu123@gmail.com>
2014-11-21 23:49:34 -0500

    Update tests for get-keys

--- test/get-keys.test.js
+++ test/get-keys.test.js
@@ -1,27 +1,22 @@
 'use strict';
 
 var expect = require('chai').expect;
-var proxyquire =  require('proxyquire').noPreserveCache().noCallThru();
+var proxyquire = require('proxyquire').noPreserveCache().noCallThru();
+var sinon = require('sinon');
+
 var Promise = require('bluebird');
 
-var rightConfig = {
-  'ca': './test/fixture/keys-docker/ca.txt',
-  'cert': './test/fixture/keys-docker/cert.txt',
-  'key': './test/fixture/keys-docker/key.txt'
-};
+var rightConfig = require('./fixture/right-ssd-config.json');
+var wrongConfig = require('./fixture/wrong-ssd-config.json')
 
-var wrongConfig = {
-  'ca': './test/fixture/keys-docker/ca.txt',
-  'cert': '',
-  'key': ''
-};
+var spy = sinon.spy();
 
-var getKeysRight = proxyquire('../lib/get-keys', {
-  './get-config': rightConfig
-});
-
+var getKeysRight = proxyquire('../lib/get-keys', {'./get-config': rightConfig});
 var getKeysWrong = proxyquire('../lib/get-keys', {
-  './get-config': wrongConfig
+  './get-config': wrongConfig,
+  './get-logger': function () {
+    return spy;
+  }
 });
 
 describe('getKeys', function () {
@@ -29,17 +24,20 @@ describe('getKeys', function () {
   it('should successfully return a promise and resolve with keys', function (done) {
     getKeysRight().then(function (keys) {
       expect(keys.length).eql(3);
+
+      // match the content of files
       expect(keys[0].toString()).eql('ca\n');
       expect(keys[1].toString()).eql('cert\n');
       expect(keys[2].toString()).eql('key\n');
-      done();
-    });
+    })
+    .finally(done);
   });
 
   it('should fail to resolve with keys', function (done) {
     getKeysWrong().catch(function (err) {
       expect(err.message).eql('Some key file cannot be located.');
-      done();
-    });
+      expect(spy.callCount).eql(2);
+    })
+    .finally(done);
   });
 });
