41d2cd652895440b77a64dc2c6afc2c3895ee78c
Daiwei <daiweilu123@gmail.com>
2014-11-22 01:35:55 -0500

    Improve get-keys tests

--- test/get-keys.test.js
+++ test/get-keys.test.js
@@ -4,40 +4,40 @@ var expect = require('chai').expect;
 var proxyquire = require('proxyquire').noPreserveCache().noCallThru();
 var sinon = require('sinon');
 
-var Promise = require('bluebird');
-
-var rightConfig = require('./fixture/right-ssd-config.json');
-var wrongConfig = require('./fixture/wrong-ssd-config.json')
-
-var spy = sinon.spy();
-
-var getKeysRight = proxyquire('../lib/get-keys', {'./get-config': rightConfig});
-var getKeysWrong = proxyquire('../lib/get-keys', {
-  './get-config': wrongConfig,
-  './get-logger': function () {
-    return spy;
-  }
-});
-
 describe('getKeys', function () {
 
   it('should successfully return a promise and resolve with keys', function (done) {
-    getKeysRight().then(function (keys) {
+    var rightConfig = require('./fixture/right-ssd-config.json');
+    var getKeys = proxyquire('../lib/get-keys', {'./ssd-config': rightConfig});
+
+    getKeys().then(function (keys) {
       expect(keys.length).eql(3);
 
       // match the content of files
       expect(keys[0].toString()).eql('ca\n');
       expect(keys[1].toString()).eql('cert\n');
       expect(keys[2].toString()).eql('key\n');
+
+      done();
     })
-    .finally(done);
+    .catch(done);
   });
 
   it('should fail to resolve with keys', function (done) {
-    getKeysWrong().catch(function (err) {
+    var wrongConfig = require('./fixture/wrong-ssd-keys-config.json');
+    var spy = sinon.spy();
+    var getKeys = proxyquire('../lib/get-keys', {
+      './ssd-config': wrongConfig,
+      './get-logger': function () {
+        return spy;
+      }
+    });
+
+    getKeys().catch(function (err) {
       expect(err.message).eql('Some key file cannot be located.');
       expect(spy.callCount).eql(2);
+      done();
     })
-    .finally(done);
+    .catch(done);
   });
 });
