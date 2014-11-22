77c4915c3650ed66b8405a670ae488060f05b0c1
Daiwei <daiweilu123@gmail.com>
2014-11-22 14:42:59 -0500

    Create clt
    - Cache docker instance in get-docker
    - Create get status method

---
+++ bin/ssd.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+var argv = require('minimist')(process.argv.slice(2));
+var ssd = require('../index');
+var log = require('../lib/get-logger')('local');
+
+switch (argv['_'][0]) {
+  case 'status':
+    ssd.status().then(function (containers) {
+      log(containers);
+    });
+    break;
+  default:
+
+}
