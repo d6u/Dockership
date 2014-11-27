aa2a3d679b862b9f3b8bacf9a3b4fec88aa22b67
Daiwei <daiweilu123@gmail.com>
2014-11-26 22:05:09 -0500

    Complete up action

--- bin/ssd.js
+++ bin/ssd.js
@@ -3,6 +3,8 @@
 var argv          = require('minimist')(process.argv.slice(2));
 var Promise       = require('bluebird');
 var semver        = require('semver');
+var _             = require('lodash');
+var async         = require('async');
 var ssd           = require('../index');
 var getConfig     = require('../lib/get-config');
 var getLogger     = require('../lib/get-logger');
@@ -13,8 +15,6 @@ var getDocker     = require('../lib/get-docker');
 
 var log   = getLogger('ssd');
 var error = getLogger('error');
-
-var $meta;
 
 switch (argv['_'][0]) {
   case 'status':
@@ -30,18 +30,75 @@ switch (argv['_'][0]) {
 }
 
 function up() {
-  Promise.all([ getConfig('meta'), ssd.getImage() ])
-    .spread(function (meta, images) {
-      $meta = meta;
-      if (images.length === 0 || semver.gt(meta.version, images[0].version)) {
-        return ssd.build();
+  getImage()
+    .then(function (image) {
+      this.image = image;
+      return ssd.getContainer();
+    })
+    .then(function (containers) {
+      this.containers = containers;
+      return _.find(containers, function (container) {
+        return this.image.RepoTags.indexOf(container.Image) > -1;
+      }, this);
+    })
+    .then(function (container) {
+      if (!container || !container.Status.match(/^Up/)) {
+        this.container = container;
+        return getDocker();
       } else {
-        var err = new Error('server has greater or same version image than local');
-        err.code = 'VERSIONERROR';
-        err.image = images[0];
-        throw err;
+        log(container);
+        throw new Error('container already running');
       }
     })
+    .then(function (docker) {
+      var _this = this;
+      removeContainerExcept(this.containers, this.container);
+      if (this.container === undefined) {
+        return docker.createContainerAsync(parseMeta(this.meta))
+          .then(function (container) {
+            return new Promise(function (resolve, reject) {
+              container.start(function (err) {
+                if (err) return reject(err);
+                resolve(container);
+              });
+            });
+          });
+      } else {
+        return new Promise(function (resolve, reject) {
+          docker.getContainer(this.container.Id).start(function (err) {
+            if (err) return reject(err);
+            resolve(_this.container);
+          });
+        });
+      }
+    })
+    .then(function (container) {
+      log('container up and running');
+      log(container);
+    })
+    .catch(function (err) {
+      error(err.message);
+    });
+}
+
+function getImage() {
+  return getConfig('meta').bind({})
+    .then(function (meta) {
+      this.meta = meta;
+      return ssd.getImage();
+    })
+    .then(function (images) {
+      this.images = images;
+      if (images.length === 0 || semver.gt(this.meta.version, images[0].version)) {
+        return buildImage();
+      } else {
+        return images[0];
+      }
+    });
+}
+
+function buildImage() {
+  return ssd.build()
     .then(function (response) {
       return new Promise(function (resolve, reject) {
         var logging = response
@@ -54,46 +111,42 @@ function up() {
       });
     })
     .then(function () {
-      log('finish building image');
+      return ssd.getImage();
     })
-    .catch(function (err) {
-      error(err.message);
-      switch(err.code) {
-        case 'BUILDERROR':
-          break;
-        case 'VERSIONERROR':
-          return startContainer(err.image);
-      }
-    })
-    .then(function (id) {
-      if (id) {
-        log(id);
-      }
+    .then(function (images) {
+      this.images = images;
+      return images[0];
     });
 }
 
-function startContainer(image) {
-  return ssd.getContainer()
-    .then(function (containers) {
-      for (var i = 0; i < containers.length; i++) {
-        if (!semver.gt($meta.version, containers[i].version)) {
-          if (containers[i].Status.match(/^Up/)) {
-            log('server already running container with same or greater version');
-            return containers[i].Id;
-          } else if (containers[i].Status.match(/^Exited/)) {
-            log('server has an existed container with same or greater version');
-            log('start that container');
-            return getDocker()
-              .then(function (docker) {
-                return new Promise(function (resolve, reject) {
-                  docker.getContainer(containers[i].Id).start(function (err) {
-                    if (err) return reject(err);
-                    resolve(containers[i].Id);
-                  });
+function removeContainerExcept(containers, container) {
+  return getDocker()
+    .then(function (docker) {
+      return new Promise(function (resolve, reject) {
+        async.each(containers, function (c, cb) {
+          if (!container || c.Id !== container.Id) {
+            var target = docker.getContainer(c.Id);
+            if (c.Status.match(/^Up/)) {
+              target.stop(function (err) {
+                if (err) return cb(err);
+                target.remove(function (err) {
+                  if (err) return cb(err);
+                  cb();
                 });
               });
+            } else {
+              target.remove(function (err) {
+                if (err) return cb(err);
+                cb();
+              });
+            }
+          } else {
+            cb();
           }
-        }
-      }
+        }, function (err) {
+          if (err) return reject(err);
+          resolve(err);
+        });
+      });
     });
 }
