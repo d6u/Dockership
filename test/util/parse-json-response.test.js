9eb13f86d9c89af2b82ffba7f92d02d2a735b60b
Daiwei <daiweilu123@gmail.com>
2015-03-11 17:48:56 -0400

    Remove unnecessary tests

--- test/util/parse-json-response.test.js
+++ test/util/parse-json-response.test.js
@@ -3,15 +3,15 @@ var expect = require('chai').expect;
 var split = require('split');
 var through2 = require('through2');
 var path = require('path');
-var fs = require('../lib/fs-promisified');
+var fs = require('../../lib/promisified/fs-promisified');
 
-var ParseJSONResponse = require('../lib/parse-json-response');
+var ParseJSONResponse = require('../../lib/util/parse-json-response');
 
-describe('PerLineStream', function () {
+describe('ParseJSONResponse', function () {
 
   it('should pipe out object contains line data and flag', function (done) {
     var i = 0;
-    fs.createReadStream(path.resolve('test/fixture/docker-response-error.txt'))
+    fs.createReadStream(path.resolve('test/fixture/_handle-build-response/docker-response-error.txt'))
       .pipe(split('<CHUNK-END>'))
       .pipe(new ParseJSONResponse())
       .pipe(through2.obj(function (obj, enc, cb) {
