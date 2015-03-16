9bba8e077ef2f7273febf6a6185bb4949e53f1c1
Daiwei <daiweilu123@gmail.com>
2015-03-15 20:02:16 -0400

    Improve tests

---
+++ test/build/_handle-build-response.test.js
@@ -0,0 +1,94 @@
+'use strict';
+
+var proxyquire = require('proxyquire').noPreserveCache().noCallThru();
+var expect = require('chai').expect;
+
+var EventEmitter = require('events').EventEmitter;
+var Stream = require('stream');
+var Readable = Stream.Readable;
+var Bluebird = require('bluebird');
+var path = require('path');
+var fs = Bluebird.promisifyAll(require('fs'));
+var BuildError = require('../../lib/errors/build-error');
+
+function createReadable(_read) {
+  var response = new Readable();
+  response._read = _read;
+  response.setTimeout = function () {};
+  return response;
+}
+
+function createResponse(fileName) {
+  var fixturePath = path.join(__dirname, '..', 'fixture', '_handle-build-response', fileName);
+  return fs
+    .readFileAsync(fixturePath, {encoding: 'utf8'})
+    .then(function (text) {
+      var pieces = text.split('<CHUNK-END>');
+      return createReadable(function () {
+        this.push(pieces.shift());
+      });
+    });
+}
+
+describe('_handleBuildResponse()', function () {
+  var _handleBuildResponse, ee;
+
+  beforeEach(function () {
+    _handleBuildResponse = proxyquire('../../lib/build/_handle-build-response', {})
+      .bind(new EventEmitter());
+  });
+
+  it('should end', function (done) {
+    createResponse('docker-response.txt')
+      .then(function (response) {
+        _handleBuildResponse(response);
+      })
+      .then(done, done);
+  });
+
+  it('should reject if response error', function (done) {
+    var response = createReadable(function () {});
+
+    _handleBuildResponse(response)
+      .then(function () {
+        done(new Error('Did not throw error'));
+      })
+      .catch(Error, function (err) {
+        expect(err.message).equal('random error');
+      })
+      .then(done, done);
+
+    response.emit('error', new Error('random error'));
+  });
+
+  it('should reject if JSON syntax error', function (done) {
+    var response = createReadable(function () {
+      this.push('not valid json }{');
+    });
+
+    _handleBuildResponse(response)
+      .then(function () {
+        done(new Error('Did not throw error'));
+      })
+      .catch(SyntaxError, function (err) {
+        expect(err.message).contains('Unexpected token');
+      })
+      .then(done, done);
+  });
+
+  it('should reject when BuildError', function (done) {
+    createResponse('docker-response-error.txt')
+      .then(function (response) {
+        return _handleBuildResponse(response);
+      })
+      .then(function () {
+        done(new Error('Did not throw error'));
+      })
+      .catch(BuildError, function (err) {
+        expect(err.message).equal('runit-app.sh: no such file or directory');
+        expect(err.details).equal('runit-app.sh: no such file or directory');
+      })
+      .then(done, done);
+  });
+
+});
